2ee6c114bffd2beb28ddc6d0e1d62d1ca2d66941
Naveen <Naveen-MBA@Naveens-MacBook-Air-775.local>
2019-02-12 19:48:44 -0500

    Now accounts for multiple rectangles

---
+++ src/main/java/frc/robot/RotatedRectangleSortLeftToRight.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import org.opencv.core.RotatedRect;
+
+public class RotatedRectangleSortLeftToRight implements Comparator<RotatedRect> {
+
+    @Override
+    public int compare(RotatedRect arg0, RotatedRect arg1) {
+        return new Double(arg0.center.x).compareTo(new Double(arg1.center.x));
+    }
+    
+}
--- src/main/java/frc/robot/VisionProcessor.java
+++ src/main/java/frc/robot/VisionProcessor.java
@@ -14,6 +14,9 @@ import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.imgproc.*;
+import java.util.*; 
+import java.lang.*; 
+import java.io.*; 
 
 /**
  * This has a lot of the code that used to be inside of GripPipeline
@@ -36,7 +39,7 @@ public class VisionProcessor implements VisionPipeline{
         parent.process(sourceFrame);
         
         ArrayList<RotatedRect> targets = minimumBoundingRectangle(parent.filterContoursOutput());
-        ArrayList<RotatedRect> nondumb = getRidOfDumbRectangles(targets);
+        ArrayList<RotatedRect> nondumb = getRidOfDumbandAloneRectangles(targets);
         //SolvePnp Implementation
         Mat rvec = CameraConstants.getRvec();
         Mat tvec = CameraConstants.getTvec();
@@ -61,16 +64,76 @@ public class VisionProcessor implements VisionPipeline{
         return distanceFromTarget;
     }
     
-    public  ArrayList<RotatedRect> getRidOfDumbRectangles(ArrayList<RotatedRect> input){
+    public  static ArrayList<RotatedRect> getRidOfDumbandAloneRectangles(ArrayList<RotatedRect> input){
         //this gets rid of rectangles in the top of the image, which are usually lights
+
         var filtered = new ArrayList<RotatedRect>();
+        var filteredFromSmall = new ArrayList<RotatedRect>();
+        var filteredFinal = new ArrayList<RotatedRect>();
+
         for ( RotatedRect rect: input){
             if ( rect.boundingRect().y > MIN_Y && rect.boundingRect().y < MAX_Y){
-                filtered.add(rect);
+                filteredFromSmall.add(rect);
+            }
+
+        }
+
+        
+        Collections.sort(filteredFromSmall, new RotatedRectangleSortLeftToRight());
+        int i = 0;
+        int len = filteredFromSmall.size();
+        
+        System.out.println("filtered_from_small"+ len);
+
+		while (i<(len-1)){
+            RotatedRect rect1 = filteredFromSmall.get(i);
+            RotatedRect rect2 = filteredFromSmall.get(i+1);
+            if (rect1.angle>0 && rect2.angle<0){
+                filtered.add(rect1);
+                filtered.add(rect2);
+                i = i + 2;
+            }
+            else{
+                i = i + 1;
             }
         }
-        return filtered;
+
+        System.out.println("filtered"+ filtered.size());
+
+        int n = 1;
+        int len_filtered = filtered.size();
+        int index_max = 0;
+        if (len_filtered>1){
+        while (n<(len_filtered-1)){
+            RotatedRect rect1 = filtered.get(n);
+            RotatedRect max_rect = filtered.get(index_max);
+            double distance_to_center1 = Math.abs(320 - rect1.center.x);
+            double distance_to_center_max = Math.abs(320 - max_rect.center.x);
+            if (distance_to_center1<distance_to_center_max){
+                index_max = n;
+            }
+            n = n+1;
+        }
+
+        System.out.println("filtered_final"+filteredFinal.size());
+
+        if (index_max==0){
+            filteredFinal.add(filtered.get(index_max));
+            filteredFinal.add(filtered.get(index_max+1)); 
+        }
+        else if (index_max%2 == 0){
+            filteredFinal.add(filtered.get(index_max));
+            filteredFinal.add(filtered.get(index_max+1));
+        }
+        else{
+            filteredFinal.add(filtered.get(index_max-1));
+            filteredFinal.add(filtered.get(index_max)); 
+        }
     }
+        return filteredFinal;
+
+    }
+    
     
     public ArrayList<RotatedRect> minimumBoundingRectangle(List<MatOfPoint> inputContours){
         //System.out.println(inputContours.size() + " inputContours");
